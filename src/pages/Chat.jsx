747fb644ffcb044fe36acd403fdaa26769c79b99
ajshan23 <ajmalshahan23@gmail.com>
2023-12-16 19:50:46 +0530

    changed bg

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import conf from "../conf/conf";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
-import { FaRobot,FaUserCircle  } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
-import img from "../image/appuimage.jpg"
+import { FaLocationPin } from "react-icons/fa6";
 import './style.css'
 import logo from '../image/umbrion.png'
 import user from '../image/user.jpg'
@@ -22,9 +20,7 @@ const Chat = () => {
     const chat = model.startChat({
       
       history: history,
-      generationConfig: {
-        maxOutputTokens: input.includes('more')? 300:200,
-      },
+      
     });
    
     const result = await chat.sendMessage(input);
@@ -56,7 +52,7 @@ const Chat = () => {
   }, []);
   return (
     <>
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full bg-[#343541]">
         <div className=" flex w-full h-20 justify-start items-center bg-[#502464]">
           <div className=" flex items-center ml-20 text-white text-2xl font-bold gap-3">
             <div>EVEE GPT</div>
@@ -66,7 +62,7 @@ const Chat = () => {
         <div className="w-screen h-screen px-20 ">
           <div className=" flex flex-col w-full h-full gap-5 mt-10">
             <div>
-              <div className=" py-2 scroll-container w-[60%] h-custom bg-opacity-10  border-[#502464] border-2 rounded-lg border-opacity-30 overflow-x-hidden overflow-y-scroll scroll-smooth" style={{backgroundImage:img}}>
+              <div className="bgstyle py-2 scroll-container w-[60%] h-custom  border-[#502464] border-2 rounded-lg border-opacity-30 overflow-x-hidden overflow-y-scroll scroll-smooth bg-cover bg-center"  >
 
 
               {
@@ -77,12 +73,13 @@ const Chat = () => {
       }`}
       key={index}
     >
-      <div className={`w-10 h-10 rounded-full flex justify-center items-center overflow-hidden ${item.role==='user'?'bg-[#E8E8E8] ml-2':' bg-[#502464] mr-2 ' }`}>
-       {item.role==='user'? <img src={user}  className="rounded" />: <FaRobot color="white" size={21} />}
+      <div className={`w-10 h-10 rounded-full flex justify-center items-center overflow-hidden ${item.role==='user'?'bg-[#E8E8E8] ml-4':' bg-white mr-4 ' }`}>
+       {item.role==='user'? <img src={user}  className="rounded w-10 h-8" />: <img src={logo} className="w-10 h-10" />}
       </div>
-      <div className={` mt-2 w-full h-full  flex ${item.role==='user' ?'bg-green-700 bg-opacity-30 rounded-tr-0 rounded-tl-lg rounded-b-lg' :'bg-[#502464] bg-opacity-30 rounded-tl-0 rounded-tr-lg rounded-b-lg'}`} >
+      <div className={`relative mt-2 w-full h-full  flex ${item.role==='user' ?'bg-[#3f4f63] rounded-tr-0 rounded-tl-lg rounded-b-lg' :'bg-[#502464] rounded-tl-0 rounded-tr-lg rounded-b-lg'}`} >
+        {item.role==='user'?<div className="absolute -rotate-90 -right-3 cuscss"><FaLocationPin color="#3f4f63" size={21}/></div>:<div className="absolute top-custom  rotate-90"><FaLocationPin color="#502464" size={21}/></div>}
         
-        <div className="px-5 py-5 text-black">{item.parts}</div>
+        <span className="px-5 py-5 text-white">{item.parts}</span>
       </div>
     </div>
   ))
@@ -96,9 +93,9 @@ const Chat = () => {
             </div>
 
             <div className=" flex flex-row w-[60%] h-14">
-              <div className="h-14 border border-[#502464] rounded-lg w-full flex justify-center items-center mr-2 border-opacity-50">
+              <div className="h-14 border border-white rounded-lg w-full flex justify-center items-center mr-2 border-opacity-50">
                 <input
-                  className="w-full mx-10 h-8 outline-none placeholder:text-[#502464] placeholder:font-thin"
+                  className="w-full bg-[#343541] mx-10 h-8 outline-none placeholder:text-white placeholder:font-thin text-white"
                   placeholder="Helloo"
                   value={input}
                   onChange={(e) => setInput(e.target.value)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
     extend: {
       height:{
         "custom":"500px"
-      }
+      },
+       
     },
   },
   plugins: [],
